ed55b50a72198e094ffd454afbdd7b1107cadcb4
Magnus Kroner <magnus.kroner@gmail.com>
2023-12-13 22:59:29 +0100

    added shortcut for fmt

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -43,7 +43,7 @@ lspcfg.rust_analyzer.setup {
             },
             checkOnSave = {
                 command = "clippy",
-            }
+            },
         }
     }
 }
@@ -82,7 +82,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
+        vim.keymap.set('n', '<leader>fm', function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
