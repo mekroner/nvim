e7aaa8b9c620d9f2e4433ca0c721d33b35296f35
Magnus Kroner <magnus.kroner@gmail.com>
2024-01-03 18:55:04 +0100

    colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,9 +5,11 @@ require('rose-pine').setup({
 function ColorMePencils(color)
     color = color or "rose-pine"
     vim.cmd.colorscheme(color)
-    
+
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
 ColorMePencils()
