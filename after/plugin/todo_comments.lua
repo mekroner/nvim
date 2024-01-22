85f4e585d547febedce4d2bbb3fe98ff7f32299e
Magnus Kroner <magnus.kroner@gmail.com>
2024-01-22 14:41:09 +0100

    installed todo_comments

---
+++ after/plugin/todo_comments.lua
@@ -0,0 +1,5 @@
+require("todo-comments").setup()
+
+-- TODO: something to do
+
+vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>")
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,48 +1,52 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     -- colorscheme
-    { 'rose-pine/neovim', as = 'rose-pine' },
-   'nvim-treesitter/nvim-treesitter',
---    -- completion
-   'hrsh7th/nvim-cmp',
-   'hrsh7th/cmp-cmdline',
-   'hrsh7th/cmp-path',
-   'hrsh7th/cmp-buffer',
-   'hrsh7th/cmp-nvim-lsp',
-   -- 'f3fora/cmp-spell',
+    { 'rose-pine/neovim',        as = 'rose-pine' },
+    'nvim-treesitter/nvim-treesitter',
+    --    -- completion
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-nvim-lsp',
+    -- 'f3fora/cmp-spell',
 
-   'nvim-tree/nvim-web-devicons',
-   {
-       'nvim-telescope/telescope.nvim', tag = '0.1.4',
-       dependencies = { 'nvim-lua/plenary.nvim' }
-   },
-   {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+    'nvim-tree/nvim-web-devicons',
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'akinsho/bufferline.nvim', version = "*",   dependencies = 'nvim-tree/nvim-web-devicons' },
 
---    -- luasnip
-   "saadparwaiz1/cmp_luasnip",
-   'L3MON4D3/LuaSnip',
-   "rafamadriz/friendly-snippets",
---    --lsp
-   "williamboman/mason.nvim",
-   "williamboman/mason-lspconfig.nvim",
-   "neovim/nvim-lspconfig",
+    -- luasnip
+    "saadparwaiz1/cmp_luasnip",
+    'L3MON4D3/LuaSnip',
+    "rafamadriz/friendly-snippets",
+    --lsp
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
     -- the goat
     'tpope/vim-commentary',
     'tpope/vim-surround',
 
-
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
 
     'lukas-reineke/indent-blankline.nvim',
 
